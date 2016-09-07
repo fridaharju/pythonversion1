41a766630da20cec63e30d59839a3320df6caa83
fridaharju <fridaharju@lifesum.com>
2016-09-07 11:40:01 +0200

    testing python
    testing testing

--- pythontesting.py
+++ pythontesting.py
@@ -22,19 +22,18 @@ book = {"price": 100, "count": 1, "tax": 1.06}
 
 print (robot["price"]*2*1.25 + book["price"] * 1 * 1.06)
 
-print (robot["price"]*(robot["count"]*(robot["tax"]+ book["price"] * book["count"] * book["tax"])
+print (robot["price"]*robot["count"]*robot["tax"]+ book["price"] * book["count"] * book["tax"])
 
 robot = Product()
-	robot.price = 900
-	robot.count = 2
-	robot.tax = 1.25
+robot.price = 900
+robot.count = 2
+robot.tax = 1.25
 
 print (
 	robot.price * robot.count * robot.tax + book.price * book.count * book.tax
 	)
 
-book = Product ()
-	book.price = 100
-	book.count = 1
-	book.tax = 1.06
-
+book = Product()
+book.price = 100
+book.count = 1
+book.tax = 1.06
